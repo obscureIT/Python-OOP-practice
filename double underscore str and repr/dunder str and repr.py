b058d642fe629aaf48264062dbda01f1513a82ce
obscureIT <67160116+obscureIT@users.noreply.github.com>
2022-04-13 06:40:59 +0600

    Create dunder str and repr.py

---
+++ double underscore str and repr/dunder str and repr.py
@@ -0,0 +1,13 @@
+class CustomizedInteger:
+    def __init__(self, integer):
+        self.integer = integer
+
+    def __str__(self):
+        if self.integer == 4:
+            return 'Four'
+
+    def __repr__(self):
+        return f'Customized integer ({self.integer})'
+
+int1 = CustomizedInteger(4)
+print(int1)
